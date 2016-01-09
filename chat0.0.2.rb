47c23c6b8ffc121de146b2428f40ae1023478718
DrMundane <nicho9las.lashinsky@gmail.com>
2016-01-09 17:37:33 -0500

    Putting 0.0.2 back up, the 2 way chat version!

---
+++ chat0.0.2.rb
@@ -0,0 +1,33 @@
+#MundaneChat 0.0.2: Now with 2 way communication!
+require 'socket'
+
+server = TCPServer.new 9876
+
+loop do
+	client = server.accept    # Wait for a client to connect
+	client.puts "Hello!"
+	client.puts "Time is #{Time.now}"
+	client.puts "Welcome to MundaneChat ver 0.0.1"
+	hn = Socket.gethostname
+	p "#{hn} has connected" #Puts the host name
+	
+	hostcom = Thread.new do
+		puts "Hostcom started" #TESTING OUTPUT <--------------------
+		status = 1
+	until status == "/exit\n"
+		status = client.gets
+		puts status
+	end
+	hostcom.kill
+	end
+	
+	servcom = Thread.new do
+		puts "Servcom started" # TESTING OUTPUT <---------------------
+		status = 1
+	until status == "/exit\n"
+		status = gets
+		client.puts status
+	end
+	servcom.kill
+	end
+end
